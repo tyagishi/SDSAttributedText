7f20895fb29962761b9bc3917fc67bc8574c7db9
Tomoaki Yagishita <tyagishi@gmail.com>
2021-04-12 14:16:38 +0900

    SDSAttributedText

--- Sources/SDSAttributedText/SDSAttributedText.swift
+++ Sources/SDSAttributedText/SDSAttributedText.swift
@@ -1,3 +1,32 @@
-struct SDSAttributedText {
-    var text = "Hello, World!"
+//
+//  SDSAttributedText.swift
+//
+//  Created by : Tomoaki Yagishita on 2021/01/06
+//  © 2021  SmallDeskSoftware
+//
+
+import Foundation
+import SwiftUI
+
+public struct SDSAttributedText: NSViewRepresentable {
+    let attributedString: NSAttributedString
+    
+    public init(_ attributedString: NSAttributedString) {
+        self.attributedString = attributedString
+    }
+    
+    public func makeNSView(context: NSViewRepresentableContext<SDSAttributedText>) -> NSTextField {
+        let textField = NSTextField(labelWithAttributedString: attributedString)
+        textField.isEditable = false
+        textField.isSelectable = false
+        textField.drawsBackground = true
+        textField.backgroundColor = NSColor.clear
+        return textField
+    }
+    
+    public func updateNSView(_ nsView: NSTextField, context: NSViewRepresentableContext<SDSAttributedText>) {
+        nsView.attributedStringValue = self.attributedString
+    }
+    
+    public typealias NSViewType = NSTextField
 }
--- Tests/SDSAttributedTextTests/SDSAttributedTextTests.swift
+++ Tests/SDSAttributedTextTests/SDSAttributedTextTests.swift
@@ -6,7 +6,7 @@ final class SDSAttributedTextTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SDSAttributedText().text, "Hello, World!")
+        XCTFail("no test")
     }
 
     static var allTests = [
